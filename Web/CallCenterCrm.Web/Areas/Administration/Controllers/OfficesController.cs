e9a3286a4474ca13c303ce2c65096222e2a48c37
susankelvin <susankelvin@users.noreply.github.com>
2014-11-13 00:56:30 +0200

    Code clean up.

--- Web/CallCenterCrm.Web/Areas/Administration/Controllers/OfficesController.cs
+++ Web/CallCenterCrm.Web/Areas/Administration/Controllers/OfficesController.cs
@@ -32,21 +32,6 @@
             return View(offices.ToList());
         }
 
-        // GET: Administration/Offices/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Office office = context.Offices.Find(id);
-            if (office == null)
-            {
-                return HttpNotFound();
-            }
-            return View(office);
-        }
-
         // GET: Administration/Offices/Create
         public ActionResult Create()
         {
