513caf2257c527e5a829e85f6e51aa71e99074f5
susankelvin <susankelvin@users.noreply.github.com>
2014-11-14 18:19:21 +0200

    Kendo UI MVC added.

--- Web/CallCenterCrm.Web/App_Start/BundleConfig.cs
+++ Web/CallCenterCrm.Web/App_Start/BundleConfig.cs
@@ -8,6 +8,34 @@ namespace CallCenterCrm.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            RegisterStyles(bundles);
+            RegisterScripts(bundles);            
+
+            bundles.IgnoreList.Clear();
+
+            BundleTable.EnableOptimizations = false;
+        }
+
+        private static void RegisterStyles(BundleCollection bundles)
+        {
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //    "~/Content/bootstrap.flatly.css",
+            //    "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                "~/Content/bootstrap.flatly.css"));
+
+            bundles.Add(new StyleBundle("~/Content/site").Include(
+                "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+                "~/Content/Kendo/kendo.common-bootstrap.min.css",
+                "~/Content/Kendo/kendo.default.min.css"));
+        }
+
+        private static void RegisterScripts(BundleCollection bundles)
+        {
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js"));
 
@@ -21,17 +49,9 @@ namespace CallCenterCrm.Web
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/respond.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //    "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                //"~/Content/bootstrap-theme.css",
-                "~/Content/bootstrap.flatly.css",
-                "~/Content/site.css"));
-
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = false;
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                "~/Scripts/Kendo/kendo.all.min.js",
+                "~/Scripts/Kendo/kendo.aspnetmvc.min.js"));
         }
     }
 }
