4614a8f72b10080c9fbf35817b95cdc1d667d665
susankelvin <susankelvin@users.noreply.github.com>
2014-11-15 21:48:11 +0200

    CallResults fixes.

--- Web/CallCenterCrm.Web/Areas/Operator/Controllers/CallResultsController.cs
+++ Web/CallCenterCrm.Web/Areas/Operator/Controllers/CallResultsController.cs
@@ -22,7 +22,6 @@
     [Authorize]
     public class CallResultsController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
         private readonly ICallCenterCrmData data;
 
         public CallResultsController(ICallCenterCrmData data) : base()
@@ -56,21 +55,6 @@
             return Json(callResults.ToDataSourceResult(request));
         }
 
-        // GET: Operator/CallResults/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CallResult callResult = db.CallResults.Find(id);
-            if (callResult == null)
-            {
-                return HttpNotFound();
-            }
-            return View(callResult);
-        }
-
         // GET: Operator/CallResults/Create
         public ActionResult Create()
         {
@@ -83,7 +67,6 @@
             };
             return View(model);
         }
-
 
         // POST: Operator/CallResults/Create
         [HttpPost]
@@ -114,86 +97,14 @@
             return View(model);
         }
 
-        // GET: Operator/CallResults/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CallResult callResult = db.CallResults.Find(id);
-            if (callResult == null)
-            {
-                return HttpNotFound();
-            }
-            //ViewBag.CampaignId = new SelectList(db.Campaigns, "CampaignId", "Product", callResult.CampaignId);
-            //ViewBag.OperatorId = new SelectList(db.ApplicationUsers, "Id", "Email", callResult.OperatorId);
-            //ViewBag.StatusId = new SelectList(db.Statuses, "StatusId", "Description", callResult.StatusId);
-            return View(callResult);
-        }
-
-        // POST: Operator/CallResults/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CallResultId,CampaignId,OperatorId,StatusId,CallDate,Duration,Notes,Customer,IsDeleted,DeletedOn")] CallResult callResult)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(callResult).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            //ViewBag.CampaignId = new SelectList(db.Campaigns, "CampaignId", "Product", callResult.CampaignId);
-            //ViewBag.OperatorId = new SelectList(db.ApplicationUsers, "Id", "Email", callResult.OperatorId);
-            //ViewBag.StatusId = new SelectList(db.Statuses, "StatusId", "Description", callResult.StatusId);
-            return View(callResult);
-        }
-
-        // GET: Operator/CallResults/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CallResult callResult = db.CallResults.Find(id);
-            if (callResult == null)
-            {
-                return HttpNotFound();
-            }
-            return View(callResult);
-        }
-
-        // POST: Operator/CallResults/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            CallResult callResult = db.CallResults.Find(id);
-            db.CallResults.Remove(callResult);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
         private List<SelectListItem> GetStatuses()
         {
             var statuses = this.data.Statuses.All()
                                .Select(s => new SelectListItem()
-                               {
-                                   Text = s.Description,
-                                   Value = s.StatusId.ToString()
-                               })
+                                      {
+                                          Text = s.Description,
+                                          Value = s.StatusId.ToString()
+                                      })
                                .ToList();
             return statuses;
         }
@@ -204,12 +115,12 @@
             ApplicationUser user = this.data.Users.Find(operatorId);
             int officeId = user.OfficeId ?? 0;
             var campaigns = this.data.Campaigns.All()
-                                .Where(c => c.OfficeId == officeId)
+                                .Where(c => c.OfficeId == officeId && c.Active)
                                 .Select(c => new SelectListItem()
-                                {
-                                    Text = c.Description,
-                                    Value = c.CampaignId.ToString()
-                                })
+                                       {
+                                           Text = c.Description,
+                                           Value = c.CampaignId.ToString()
+                                       })
                                 .ToList();
             return campaigns;
         }
