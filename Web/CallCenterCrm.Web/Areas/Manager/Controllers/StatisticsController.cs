9ff61463e4d042166195c5dda02fbaa20d4889eb
susankelvin <susankelvin@users.noreply.github.com>
2014-11-16 21:34:08 +0200

    Campaign statistics implemented.

--- Web/CallCenterCrm.Web/Areas/Manager/Controllers/StatisticsController.cs
+++ Web/CallCenterCrm.Web/Areas/Manager/Controllers/StatisticsController.cs
@@ -9,11 +9,10 @@
     using System.Web;
     using System.Web.Mvc;
     using CallCenterCrm.Data;
-    using CallCenterCrm.Data.Models;
+    using CallCenterCrm.Web.Areas.Manager.Models.Statistics;
 
     public class StatisticsController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
         private readonly ICallCenterCrmData data;
 
         public StatisticsController(ICallCenterCrmData data)
@@ -22,113 +21,50 @@
         }
 
         // GET: Manager/Status
-        public ActionResult Index()
+        public ActionResult Index(int? campaingId)
         {
-            return View(db.Statuses.ToList());
+            if (campaingId == null)
+            {
+                campaingId = 1;
+            }
+
+            CampaignStatisticsModel campaignStatistics = GetCampaignStatistics(campaingId ?? 0);
+            IndexStatisticsModel result = new IndexStatisticsModel();
+            result.CampaignStatistics = new[] { campaignStatistics };
+
+            var campaings = this.data.Campaigns.All()
+                                .Select(c => new SelectListItem()
+                                       {
+                                           Text = c.Description,
+                                           Value = c.CampaignId.ToString()
+                                       })
+                                .ToList();
+            result.CampaingId = campaingId ?? 0;
+            result.Campaigns = campaings;
+            return View(result);
         }
 
-        // GET: Manager/Status/Details/5
-        public ActionResult Details(int? id)
+        private CampaignStatisticsModel GetCampaignStatistics(int campaingId)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Status status = db.Statuses.Find(id);
-            if (status == null)
-            {
-                return HttpNotFound();
-            }
-            return View(status);
-        }
-
-        // GET: Manager/Status/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Manager/Status/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "StatusId,Description,IsDeleted,DeletedOn")] Status status)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Statuses.Add(status);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(status);
-        }
-
-        // GET: Manager/Status/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Status status = db.Statuses.Find(id);
-            if (status == null)
-            {
-                return HttpNotFound();
-            }
-            return View(status);
-        }
-
-        // POST: Manager/Status/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "StatusId,Description,IsDeleted,DeletedOn")] Status status)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(status).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(status);
-        }
-
-        // GET: Manager/Status/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Status status = db.Statuses.Find(id);
-            if (status == null)
-            {
-                return HttpNotFound();
-            }
-            return View(status);
-        }
-
-        // POST: Manager/Status/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Status status = db.Statuses.Find(id);
-            db.Statuses.Remove(status);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            var calls = this.data.CallResults.All()
+                           .Where(c => c.CampaignId == campaingId);
+            int callsCount = calls.Count();
+            int statusSoldId = this.data.Statuses.All()
+                               .Where(s => s.Description == "Sold")
+                               .FirstOrDefault()
+                               .StatusId;
+            var soldCalls = calls.Where(c => c.StatusId == statusSoldId);
+            int soldCount = soldCalls.Count();
+            CampaignStatisticsModel campaignStatistics = new CampaignStatisticsModel();
+            campaignStatistics.SoldToTotalCalls = (decimal)soldCount / callsCount;
+            int callsTotalTime = calls.Sum(c => c.Duration);
+            campaignStatistics.SoldToTotalTime = (decimal)soldCount / callsTotalTime;
+            decimal price = this.data.Campaigns.Find(campaingId).Price * soldCount;
+            campaignStatistics.TotelSoldPriceToTotalTime = callsTotalTime / price;
+            int notAnswered = calls.Where(c => c.Status.Description == "No answer")
+                                  .Count();
+            campaignStatistics.NotAnsweredToTotalCalls = (decimal)notAnswered / callsCount;
+            return campaignStatistics;
         }
     }
 }
---
+++ Web/CallCenterCrm.Web/Areas/Manager/Models/Statistics/IndexStatisticsModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CallCenterCrm.Web.Areas.Manager.Models.Statistics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    public class IndexStatisticsModel
+    {
+        public IEnumerable<CampaignStatisticsModel> CampaignStatistics { get; set; }
+
+        [Display(Name="Campaigns")]
+        public int CampaingId { get; set; }
+
+        public IEnumerable<SelectListItem> Campaigns { get; set; }
+    }
+}
