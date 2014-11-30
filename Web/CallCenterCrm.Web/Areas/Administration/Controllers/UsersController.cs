e005f48aaf68f5621cb7d7528fad8c818c28da74
susankelvin <susankelvin@users.noreply.github.com>
2014-12-01 01:56:17 +0200

    Code clean up.

--- Web/CallCenterCrm.Web/Areas/Administration/Controllers/UsersController.cs
+++ Web/CallCenterCrm.Web/Areas/Administration/Controllers/UsersController.cs
@@ -19,7 +19,6 @@
     public class UsersController : Controller
     {
         private const int PAGE_SIZE = 10;
-        private readonly ApplicationDbContext context = new ApplicationDbContext();
         private readonly ICallCenterCrmData data;
 
         public UsersController(ICallCenterCrmData data)
@@ -47,23 +46,6 @@
             return View();
         }
 
-        // POST: Administration/Users/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,OfficeId,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser applicationUser)
-        {
-            if (ModelState.IsValid)
-            {
-                context.Users.Add(applicationUser);
-                context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(applicationUser);
-        }
-
         // GET: Administration/Users/Edit/5
         public ActionResult Edit(string id)
         {
@@ -71,26 +53,28 @@
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ApplicationUser user = context.Users.Find(id);
+
+            ApplicationUser user = this.data.Users.Find(id);
             if (user == null)
             {
                 return HttpNotFound();
             }
 
-            List<SelectListItem> offices = context.Offices.Select(o => new SelectListItem()
-            {
-                Text = o.Name,
-                Value = o.OfficeId.ToString()
-            }).ToList();
-
-            List<SelectListItem> roles = context.Roles.Select(r => new SelectListItem()
-            {
-                Text = r.Name,
-                Value = r.Id
-            }).ToList();
-
+            List<SelectListItem> offices = this.data.Offices.All()
+                .Select(o => new SelectListItem()
+                {
+                    Text = o.Name,
+                    Value = o.OfficeId.ToString()
+                })
+                .ToList();
+            List<SelectListItem> roles = this.data.Context.Roles
+                .Select(r => new SelectListItem()
+                {
+                    Text = r.Name,
+                    Value = r.Id
+                })
+                .ToList();
             string roleId = user.Roles.First().RoleId;
-
             EditUserViewModel model = new EditUserViewModel()
             {
                 Id = user.Id,
@@ -102,27 +86,24 @@
                 RoleId = roleId,
                 Roles = roles
             };
-
             return View(model);
         }
 
         // POST: Administration/Users/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(EditUserViewModel model)
         {
             if (ModelState.IsValid)
             {
-                ApplicationUser user = this.context.Users.Find(model.Id);
+                ApplicationUser user = this.data.Users.Find(model.Id);
                 if (user == null)
                 {
                     this.TempData["ErrorMessage"] = "Invalid user id";
                     this.RedirectToAction("Index");
                 }
 
-                var role = this.context.Roles.Find(model.RoleId);
+                var role = this.data.Context.Roles.Find(model.RoleId);
                 if (role == null)
                 {
                     this.TempData["ErrorMessage"] = "Invalid role";
@@ -130,11 +111,10 @@
                 }
 
                 user.Roles.Clear();
-
                 user.PhoneNumber = model.PhoneNumber;
                 user.OfficeId = model.OfficeId;
-                this.context.Entry(user).State = EntityState.Modified;
-                context.SaveChanges();
+                this.data.Users.Update(user);
+                this.data.SaveChanges();
 
                 var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
                 userManager.AddToRole(user.Id, role.Name);
@@ -150,30 +130,22 @@
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(string id)
         {
-            ApplicationUser applicationUser = context.Users.Find(id);
-            context.Users.Remove(applicationUser);
-            context.SaveChanges();
+            ApplicationUser applicationUser = this.data.Users.Find(id);
+            this.data.Users.Delete(applicationUser);
+            this.data.SaveChanges();
             return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                context.Dispose();
-            }
-            base.Dispose(disposing);
         }
 
         private IndexViewModel FilterUsers(string tbSearch, int? pageIndex)
         {
             string search = tbSearch.ToLower();
-            var users = context.Users.Include("Office")
+            var users = this.data.Users.All()
+                .Include("Office")
                 .Where(u => u.UserName.Contains(search) || u.Email.Contains(search));
 
             int usersCount = users.Count();
-            int pageCount = usersCount / PAGE_SIZE;
-            if (usersCount % PAGE_SIZE != 0)
+            int pageCount = usersCount/PAGE_SIZE;
+            if (usersCount%PAGE_SIZE != 0)
             {
                 pageCount++;
             }
@@ -189,14 +161,14 @@
             }
 
             users = users.OrderBy(u => u.Id)
-                .Skip((int)pageIndex * PAGE_SIZE)
+                .Skip((int) pageIndex*PAGE_SIZE)
                 .Take(PAGE_SIZE);
 
             var usersList = users.Select(UserViewModel.FromUser)
                 .ToList();
             foreach (var user in usersList)
             {
-                IdentityRole role = context.Roles.Find(user.Role);
+                IdentityRole role = this.data.Context.Roles.Find(user.Role);
                 user.Role = role != null ? role.Name : "";
             }
 
